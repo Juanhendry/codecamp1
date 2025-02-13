645643ab6a79366d862560c3a76d232645a97597
Juanhendry <54432524+Juanhendry@users.noreply.github.com>
2025-02-13 03:50:45 +0000

    Assistant checkpoint: Fix test suite structure and add proper closures
    Assistant generated file changes:
    - tests/2_functional-tests.js: Fix test suite structure and proper closure
    
    ---
    
    User prompt:
    
    SyntaxError: Unexpected end of input
        at wrapSafe (node:internal/modules/cjs/loader:1378:20)
        at Module._compile (node:internal/modules/cjs/loader:1428:41)
        at Module._extensions..js (node:internal/modules/cjs/loader:1548:10)
        at Module.load (node:internal/modules/cjs/loader:1288:32)
        at Module._load (node:internal/modules/cjs/loader:1104:12)
        at Module.require (node:internal/modules/cjs/loader:1311:19)
        at require (node:internal/modules/helpers:179:18)
        at /home/runner/workspace/node_modules/mocha/lib/mocha.js:430:36
        at Array.forEach (<anonymous>)
        at Mocha.loadFiles (/home/runner/workspace/node_modules/mocha/lib/mocha.js:427:14)

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,3 +1,4 @@
+
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -16,7 +17,6 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          
           assert.equal(res.status, 200);
           assert.equal(res.text, 'hello Guest');
           done();
@@ -42,70 +42,81 @@ suite('Functional Tests', function () {
         .put('/travellers')
         .send({surname: "Colombo"})
         .end(function (err, res) {
-               /** your tests here **/
-      assert.equal(res.status, 200, 'response status should be 200');
-      assert.equal(res.type, 'application/json', 'Response should be json');
-      assert.equal(
-        res.body.name,
-        'da Verrazzano',
-        'res.body.name should be "da Verrazzano"'
-      );
-      assert.equal(
-        res.body.surname,
-        'Colombo',
-        'res.body.surname should be "Colombo"'
-      );
-
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(
+            res.body.name,
+            'da Verrazzano',
+            'res.body.name should be "da Verrazzano"'
+          );
+          assert.equal(
+            res.body.surname,
+            'Colombo',
+            'res.body.surname should be "Colombo"'
+          );
           done();
         });
     });
     // #4
-  test('send {surname: "da Verrazzano"}', function(done) {
-  /** place the chai-http request code here... **/
-  chai
-    .request(server)
-    .put('/travellers')
-    .send({ surname: 'da Verrazzano' })
-    /** place your tests inside the callback **/
-    .end(function(err, res) {
-      assert.equal(res.status, 200, 'response status should be 200');
-      assert.equal(res.type, 'application/json', 'Response should be json');
-      assert.equal(res.body.name, 'Giovanni');
-      assert.equal(res.body.surname, 'da Verrazzano');
-
-      done();
+    test('send {surname: "da Verrazzano"}', function(done) {
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({ surname: 'da Verrazzano' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
+          done();
+        });
     });
+  });
+
+  const Browser = require('zombie');
+  Browser.site = 'https://boilerplate-mochachai.your-username.repl.co';
+  const browser = new Browser();
+
+  suite('Functional Tests with Zombie.js', function () {
+    this.timeout(5000);
+
+    suite('Headless browser', function () {
+      test('should have a working "site" property', function() {
+        assert.isNotNull(browser.site);
+      });
+    });
+
+    suite('"Famous Italian Explorers" form', function () {
+      // #5
+      test('Submit the surname "Colombo" in the HTML form', function (done) {
+        browser.fill('surname', 'Colombo').then(() => {
+          browser.pressButton('submit', () => {
+            browser.assert.success();
+            browser.assert.text('span#name', 'Marco');
+            browser.assert.text('span#surname', 'Colombo');
+            browser.assert.element('span#dates', 1);
+            done();
+          });
+        });
+      });
+      // #6
+      test('Submit the surname "Vespucci" in the HTML form', function (done) {
+        browser.fill('surname', 'Vespucci').then(() => {
+          browser.pressButton('submit', () => {
+            browser.assert.success();
+            browser.assert.text('span#name', 'Amerigo');
+            browser.assert.text('span#surname', 'Vespucci');
+            browser.assert.element('span#dates', 1);
+            done();
+          });
+        });
+      });
+    });
+  });
 });
-  });
-const Browser = require('zombie');
 
-suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
-
-
-
-  suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
-    });
-  });
-
-  suite('"Famous Italian Explorers" form', function () {
-    // #5
-    test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
-    });
-    // #6
-    test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
-    });
-  });
-});
 after(function() {
   chai.request(server)
-  .get('/')
+    .get('/')
+    .end();
 });
